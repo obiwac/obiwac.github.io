24f056daa3bfe22839965fe739e82f36df0376c2
obiwac <obiwac@gmail.com>
2023-05-26 18:49:49 +0200

    rocket: Basic content wrapper

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,45 @@
 #[macro_use] extern crate rocket;
 
 extern crate maud;
-use maud::{html, Markup};
+use maud::{html, Markup, DOCTYPE};
+
+fn wrap(content: Markup) -> Markup {
+	html! {
+		(DOCTYPE)
+
+		head {
+			meta charset="UTF-8"; // must be in the first 1024 bytes of the document
+			meta name="description" content="Personal website for Aymeric Wibo"; // can't be longer than 275 characters as per Google's 2017 limit on the SERP
+			meta name="viewport" content="width=device-width,initial-scale=1";
+			meta name="robots" content="index,follow";
+			meta name="theme-color" content="#000000";
+
+			link rel="icon" type="image/png" href="/icon.png";
+			link rel="manifest" href="manifest.json";
+
+			// Apple PWA stuff
+
+			meta name="apple-mobile-web-app-capable" content="yes";
+			meta name="apple-mobile-web-app-status-bar-style" content="black-translucent";
+			meta name="apple-mobile-web-app-title" content="De Bird";
+
+			// TODO keywords, google-site-verification, apple-touch-startup-image
+
+			title { "Aymeric Wibo" }
+			link rel="stylesheet" type="text/css" href="/style.css";
+		}
+
+		body {
+			(content)
+		}
+	}
+}
 
 #[get("/")]
 fn index() -> Markup {
-	html! {
+	wrap(html! {
 		h1 { "Header" }
-		p { "Paragraph" }
-	}
+	})
 }
 
 #[launch]
