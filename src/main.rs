0a55317373a2b836a14e0ecef6b487b302f1a61b
obiwac <obiwac@gmail.com>
2023-05-26 23:11:43 +0200

    rocket: Add portfolio etc etc

--- src/main.rs
+++ src/main.rs
@@ -60,13 +60,103 @@ fn index() -> Markup {
 			p {
 				"My name is "
 				strong { "Aymeric Wibo" }
-				", a Belgian open source software fanatic! My current main projects are "
-				a.link href="/aquabsd" { "aquaBSD" }
-				", which is an OS forked from FreeBSD geared towards general users, and a "
-				a.link href="/mcpy" { "video tutorial series" }
-				" on 3D graphics programming in Python."
+				", AKA "
+				strong { "obiwac" }
+				"."
+				br;
+				"I'm a Belgian open-source enthusiast who likes dogs and beer 🍺"
 			}
-			p { "Here are my socials:" }
+			p { "Here are a few of my projects:" }
+			.things {
+				.thing {
+					.labeled-img {
+						img src="https://user-images.githubusercontent.com/11079650/155240444-53454627-84f0-4a52-81aa-9eb60f8770e8.png";
+						div {
+							h2 { "aquaBSD" }
+						}
+					}
+					p { "OS forked from FreeBSD geared towards general users." }
+				}
+				.thing {
+					.labeled-img {
+						// iframe src="https://drakeerv.github.io/js-minecraft-clone/episodes/episode-5/index.html" {}
+						img src="https://github.com/obiwac/python-minecraft-clone/blob/master/eyecandy/creeper.png?raw=true";
+						div {
+							h2 { "MCPY" }
+						}
+					}
+					p { "Minecraft clone written in Python. Video tutorial series on 3D graphics programming." }
+				}
+				.thing {
+					.labeled-img {
+						img src="https://github.com/karwa-org/karwa2023/blob/main/logo.png?raw=true";
+						div {
+							h2 { "KARWa '23" }
+						}
+					}
+					p { "Francophone algorithmics contest. Jointly organized by Louvain-li-Nux (in Louvain-la-Neuve) and CPUMons (in Mons)." }
+				}
+				.thing {
+					.labeled-img {
+						img src="https://github.com/obiwac/bfm/raw/main/images/naive.gif";
+						div {
+							h2 { "BFM" }
+						}
+					}
+					p {
+						"Big F'ing Matrix. FEM/FEA C library ("
+						code { "libbfm" }
+						") with Python bindings ("
+						code { "pybfm" }
+						") for use as an educational tool. Alex and I made this for LEPL1110."
+					}
+				}
+				.thing {
+					.labeled-img {
+						img src="https://github.com/obiwac/lln-gamejam-2023/raw/main/eyecandy/obamatriangle.jpg";
+						div {
+							h2 { "LLN 2023" }
+						}
+					}
+					p { "Submission for the 2023 Louvain-li-Nux gamejam. AKA Alexis and I's first foray into Vulkan and Rust. AKA Obamatriangle." }
+				}
+				.thing {
+					.labeled-img {
+						img src="https://github.com/obiwac/lln-gamejam-2022/raw/main/eyecandy/volcano-look.png";
+						div {
+							h2 { "LLN 2022" }
+						}
+					}
+					p { "Submission for the 2022 Louvain-li-Nux gamejam. Pure C11, X11, 7/11." }
+				}
+				.thing {
+					.labeled-img {
+						img src="https://github.com/obiwac/x-compositing-wm/raw/main/pics/screenshot1.png";
+						div {
+							h2 { "x-compositing-wm" }
+						}
+					}
+					p { "Extremely basic X11 compositing window manager written in C with Xlib and OpenGL." }
+				}
+				.thing {
+					.labeled-img {
+						img src="https://github.com/NovAti0n/MOOdle/raw/main/eyecandy/paturage.png";
+						div {
+							h2 { "MOOdle" }
+						}
+					}
+					p { "Advanced cow visualization tool." }
+				}
+				.thing {
+					.labeled-img {
+						img src="https://github.com/NovAti0n/GDPR-presentation/raw/main/screenshot.png";
+						div {
+							h2 { "GDPR" }
+						}
+					}
+					p { "Interactive GDPR presentation Noa and I made in English class in highschool." }
+				}
+			}
 			.socials {
 				a.social href="https://www.linkedin.com/in/awibo" {
 					(include_static!("/static/icons/linkedin.svg"))
