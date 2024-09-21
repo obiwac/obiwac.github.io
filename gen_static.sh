63086a4ccb04506574a9185098278074026511e5
Aymeric Wibo <obiwac@gmail.com>
2024-09-21 10:58:01 +0100

    gen_static: Use `curl` instead of `fetch`

--- gen_static.sh
+++ gen_static.sh
@@ -26,7 +26,7 @@ sleep 2 # XXX should be enough, lol
 
 for route in $ROUTES; do
 	mkdir -p static$route
-	fetch http://localhost:8000$route -o static$route/index.html
+	curl http://localhost:8000$route > static$route/index.html
 done
 
 kill $(jobs -p)
